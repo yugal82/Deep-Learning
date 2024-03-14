c596db8d254ec5a09a8ce58129feab0cf9f9e071
Yugal Khanter <yugalkhanter82@gmail.com>
2024-03-14 19:33:04 +0530

    CNN architecture implemented

--- Project/Potato-Disease/model/model.py
+++ Project/Potato-Disease/model/model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras import models, layers
+from tensorflow import keras
+from keras import models, layers
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -72,3 +73,67 @@ data_augmentation = tf.keras.Sequential([
     layers.RandomFlip("horizontal_and_vertical"),
     layers.RandomRotation(0.2)
 ])
+
+input_shape = (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
+n_classes = 3
+
+model = models.Sequential([
+    resize_and_rescale,
+    data_augmentation,
+
+    # conv layers
+    layers.Conv2D(filters=32, kernel_size=(3,3), activation='relu', input_shape=input_shape),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+
+    layers.Flatten(),
+    
+    layers.Dense(64, activation='relu'),
+    layers.Dense(n_classes, activation='softmax')
+])
+
+model.build(input_shape=input_shape)
+
+model.summary()
+
+model.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=["accuracy"]
+)
+
+history = model.fit(
+    train_ds,
+    epochs=EPOCHS,
+    batch_size=BATCH_SIZE,
+    verbose=1,
+    validation_data=validation_ds
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.plot(range(EPOCHS), acc, label="Training Accuracy")
+plt.plot(range(EPOCHS), val_acc, label="Validation Accuracy")
+plt.legend(loc="lower right")
+plt.title("Training and Validation Accuracy")
+
+plt.subplot(1,2,2)
+plt.plot(range(EPOCHS), loss, label="Training Loss")
+plt.plot(range(EPOCHS), val_loss, label="Validation Loss")
+plt.legend(loc="upper right")
+plt.title("Training and Validation Loss")
